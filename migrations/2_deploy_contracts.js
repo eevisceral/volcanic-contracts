121367e09294a1035c3c68d92a2483878bac35fc
eevisceral <eevisceral@protonmail.com>
2021-11-17 01:46:20 -0500

    update chain ids and contract addresses on testnet

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,24 @@
+// require('dotenv').config();
+var Multicall = artifacts.require("./Multicall");
+var Lava = artifacts.require("./cake");
+var LavaMasterChef = artifacts.require("./masterchef");
+const admin = '0x87e42684b1c1468f18e09342ea803f34b917ef32';
+
+// module.exports = function(deployer) {
+//   deployer.deploy(Lava).then(function(){
+//         return deployer.deploy(LavaMasterChef, Lava.address, admin, admin, "1000000000000000000", 4)
+// });
+// };
+
+module.exports = function (deployer) {
+  // deploy Multicall first
+  deployer.deploy(Multicall);
+  // deploy Lava ERC20 before calling it in MasterChef
+  deployer.deploy(Lava).then(async () => {
+    // get JS instance of deployed ERC20 contract
+    const lavaInstance = await Lava.deployed();
+    // pass its address as argument for MasterChef's constructors
+    await deployer.deploy(LavaMasterChef, Lava.address, admin, admin, "1000000000000000000", 4813211);
+                          // {_tokenAddress, _devaddr, _feeAddress, _LavaPerBlock, _startBlock}
+  });
+};
